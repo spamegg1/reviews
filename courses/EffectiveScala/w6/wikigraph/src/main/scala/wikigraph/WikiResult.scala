7b7ff87b2ee98ae4f5b971edab9fbec0bb9644e2
spamegg <spamegg@pm.me>
2021-07-23 20:24:38 +0300

    finished Effective Scala

--- courses/EffectiveScala/w6/wikigraph/src/main/scala/wikigraph/WikiResult.scala
+++ courses/EffectiveScala/w6/wikigraph/src/main/scala/wikigraph/WikiResult.scala
@@ -1,57 +1,61 @@
 package wikigraph
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 import wikigraph.errors.WikiError
 import wikigraph.errors.WikiException
 
 /**
   * The result of an asynchronous computation which may fail.
-  * This class is wrapper of Future[Either[Seq[WikiError], A]]
+  * This class is wrapper of `Future[Either[Seq[WikiError], A]]`.
   *
-  * @param A the type of the result of a successful computation
+  * @tparam A the type of the result of a successful computation
   * @param value the future to use to create this WikiResult. Checkout
   *        the companion object to find other ways to create a WikiResult
   * 
   * A WikiResult, after it completes, can be in one of the following states:
   * 
-  *  - [[Success(Right(result))]]
-  *  - [[Failure(exception)]]
-  *  - [[Success(Left(Seq(error)))]]
+  *  - success: `Success(Right(result))`
+  *  - system failure: `Failure(exception)`
+  *  - domain error: `Success(Left(Seq(error)))`
   */
-case class WikiResult[A](val value: Future[Either[Seq[WikiError], A]]):
+case class WikiResult[A](value: Future[Either[Seq[WikiError], A]]):
 
   /**
-    * If the computation fails, use the provided value as a result
-    * 
+    * Recovers from domain errors by returning the provided successful `solution` value instead.
+    * If this WikiResult is a system failure, that failure is not recovered.
+    *
     * @param solution the value to use in case of failure
     */
   def orElse(solution: A)(using ExecutionContext): WikiResult[A] =
     val f = value.map {
-      case Left(_) => Right(solution)
-      case nofail@Right(_)=> nofail
+      case Left(_)           => Right(solution)
+      case nofail @ Right(_) => nofail
     }
     WikiResult(f)
 
   /**
-    * If the computation fails, use the result of the provided computation
-    * 
+    * Tries to recover from domain errors by using `that` WikiResult instead.
+    * If both `this` and `that` are domain errors, only the errors of `that`
+    * are returned.
+    * If this WikiResult is a system failure, that failure is not recovered.
+    *
     * @param b the computation to use in case of failure
     */
   def fallbackTo(that: => WikiResult[A])(using ExecutionContext): WikiResult[A] =
-    val f = value.flatMap {
-      case Left(_) => that.value
-      case Right(_) => value
+    val f = this.value.flatMap {
+      case Left(_)  => that.value
+      case Right(_) => this.value
     }
     WikiResult(f)
 
   /**
-    * Transform the result of the computation
+    * Transform the successful result of this computation
     * 
     * @param f the function used to transform the result of the computation
     * 
     * Hint: Both Either and Future have a similar method
     */
-  def map[B](f: A => B)(using ExecutionContext): WikiResult[B] =
+  def map[B](f: A => B)(using ExecutionContext): WikiResult[B] = // TODO
     WikiResult(value.map(x => x.map(f)))
 
   /**
@@ -64,7 +68,7 @@ case class WikiResult[A](val value: Future[Either[Seq[WikiError], A]]):
     *       error should be propagated
     */
   def flatMap[B](f: A => WikiResult[B])(using ExecutionContext): WikiResult[B] = 
-    val futureB: Future[Either[Seq[WikiError], B]] = value.flatMap {
+    val futureB: Future[Either[Seq[WikiError], B]] = value.flatMap { // TODO
       case Left(seqA) => Future(Left(seqA))
       case Right(a) => f(a).value
     }
@@ -80,12 +84,12 @@ case class WikiResult[A](val value: Future[Either[Seq[WikiError], A]]):
     * Hint: The async part has been handled for you. You need to zip the two Either 
     */
   def zip[B](that: WikiResult[B])(using ExecutionContext): WikiResult[(A, B)] =
-    def zipEithersAcc(a: Either[Seq[WikiError], A], b: Either[Seq[WikiError], B]): Either[Seq[WikiError], (A, B)] =
-      (a, b) match
-        case (Left(seqA), Left(seqB)) => Left(seqA ++ seqB)
-        case (Left(seqA), Right(y)) => Left(seqA)
-        case (Right(x), Left(seqB)) => Left(seqB)
-        case (Right(x), Right(y)) => Right((x, y))
+    def zipEithersAcc(a: Either[Seq[WikiError], A], b: Either[Seq[WikiError], B])
+    : Either[Seq[WikiError], (A, B)] = (a, b) match // TODO
+      case (Left(seqA), Left(seqB)) => Left(seqA ++ seqB)
+      case (Left(seqA), Right(y)) => Left(seqA)
+      case (Right(x), Left(seqB)) => Left(seqB)
+      case (Right(x), Right(y)) => Right((x, y))
     WikiResult(this.value.flatMap { thisEither =>
       that.value.map { thatEither =>
         zipEithersAcc(thisEither, thatEither)
@@ -99,28 +103,32 @@ object WikiResult:
     * @param A the type of the result
     * @param a the value of the result
     */
-  def successful[A](a: A): WikiResult[A] = WikiResult(Future.successful(Right[Seq[WikiError], A](a)))
+  def successful[A](a: A): WikiResult[A] =
+    WikiResult(Future.successful(Right[Seq[WikiError], A](a)))
 
   /**
     * Creates a WikiResult which fails with the provided domain error
     * 
     * @param e the domain error which causes the failure of the computation
     */
-  def domainError[A](e: WikiError): WikiResult[A] = WikiResult(Future.successful(Left[Seq[WikiError], A](Seq(e))))
+  def domainError[A](e: WikiError): WikiResult[A] =
+    WikiResult(Future.successful(Left[Seq[WikiError], A](Seq(e))))
 
   /**
     * Creates a WikiResult which fails with the provided system exception
     * 
     * @param t the throwable exception causing the failure of the computation
     */
-  def systemFailure[A](t: WikiException): WikiResult[A] = WikiResult(Future.failed(t))
+  def systemFailure[A](t: WikiException): WikiResult[A] =
+    WikiResult(Future.failed(t))
 
   /**
     * Starts running the provided block asynchronously in the contextual execution context
     * 
     * @param block the computation to execute
     */
-  def start(block: => Unit)(using ExecutionContext): WikiResult[Unit] = WikiResult(Future(Right(block)))
+  def start(block: => Unit)(using ExecutionContext): WikiResult[Unit] =
+    WikiResult(Future(Right(block)))
 
   /**
     * Asynchronously and non-blockingly transforms a `Seq[A]` into a WikiResult[Seq[B]]
@@ -141,7 +149,8 @@ object WikiResult:
     * all the `WikiResult` values with the operation `zip`. If the input sequence is
     * empty, return a successful empty sequence.
     */
-  def traverse[A, B](as: Seq[A])(f: A => WikiResult[B])(using ExecutionContext): WikiResult[Seq[B]] =
+  def traverse[A, B](as: Seq[A])(f: A => WikiResult[B])(using ExecutionContext)
+  : WikiResult[Seq[B]] = // TODO
     val initVal: WikiResult[Seq[B]] = WikiResult.successful(Seq.empty)
     def iterFun(seqB: WikiResult[Seq[B]], a: A): WikiResult[Seq[B]] =
       seqB.zip(f(a)).map((bs, b) => bs :+ b)
--- courses/EffectiveScala/w6/wikigraph/src/main/scala/wikigraph/Wikigraph.scala
+++ courses/EffectiveScala/w6/wikigraph/src/main/scala/wikigraph/Wikigraph.scala
@@ -2,6 +2,7 @@ package wikigraph
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+
 import Articles.ArticleId
 
 
@@ -19,13 +20,12 @@ final class Wikigraph(client: Wikipedia):
     * 
     * Hint: Use the methods that you implemented in WikiResult.
     */
-  def namedLinks(of: ArticleId): WikiResult[Set[String]] =
+  def namedLinks(of: ArticleId): WikiResult[Set[String]] = // TODO
     val linkSet: WikiResult[Set[ArticleId]] = client.linksFrom(of)
     val linkSeq: WikiResult[Seq[ArticleId]] = linkSet.map(_.toSeq)
     val traversedSeq: WikiResult[Seq[String]] =
       linkSeq.flatMap(list => WikiResult.traverse(list)(client.nameOfArticle))
     traversedSeq.map(_.toSet)
-
 
   /**
     * Computes the distance between two articles using breadth first search.
@@ -38,12 +38,15 @@ final class Wikigraph(client: Wikipedia):
     *         is exceeded during the search, the result is None
     * 
     * Note: if a domain error occurs when jumping from node to node,
-    *       fallback by ignoring the problematic node
+    *       fallback by ignoring the problematic node. On the other hand,
+    *       any system failure just ends the algorithm by returning that
+    *       system failure.
     * 
     * Hint: More information is provided in the description of the assignment
-    *       Use the enqueue and dequeue methods of Queue.
+    *       Use the `enqueue` and `dequeue` methods of `Queue`.
     */
-  def breadthFirstSearch(start: ArticleId, target: ArticleId, maxDepth: Int): WikiResult[Option[Int]] =
+  def breadthFirstSearch(start: ArticleId, target: ArticleId, maxDepth: Int)
+  : WikiResult[Option[Int]] = // TODO
     import scala.collection.immutable.Queue
     /**
       * This recursive method iterates on the graph.
@@ -59,24 +62,53 @@ final class Wikigraph(client: Wikipedia):
       * @param visited keep the nodes the are already visited, used no to iterate infinitely on
       *        graph cycles
       * @param q the next nodes to visit and their distance from `start`
+      *
+      * Issues 1 and 2: you want to use flatMap here, so that you can write the
+      * continuation of your program in the function passed to flatMap, which
+      * takes a Set[ArticleId], and returns a successful WikiResult if the
+      * neighbor nodes contain the target, or it returns the result of
+      * recursively calling iter, with the updated queue. (so, the recursive
+      * call to iter is performed within the function passed to flatMap)
+      * Issue 3: I think the problem comes from our side here. I would say that
+      * the instructions could be clearer, and the specification of the methods
+      * fallbackTo and orElse could be clarified further as well. What we should
+      * add in the instructions (I’m going to do it as I write this message), is
+      * that any system failure will stop the algorithm, which would return that
+      * failure, on the other hand, domain errors should not stop the algorithm,
+      * which would just skip that node and iterate on the remaining ones. Both
+      * orElse and fallbackTo method recover from domain errors, so you will use
+      * one of these methods, depending on which one works better for you given
+      * its type signature.
       */
-    def iter(visited: Set[ArticleId], q: Queue[(Int, ArticleId)]): WikiResult[Option[Int]] =
+    def iter(visited: Set[ArticleId], q: Queue[(Int, ArticleId)])
+    : WikiResult[Option[Int]] = // TODO
+
       if q.isEmpty then
         WikiResult.successful(None)
+
       else
-        val (node, queue): ((Int, ArticleId), Queue[(Int, ArticleId)]) =
+        val ((depth, aId), queue): ((Int, ArticleId), Queue[(Int, ArticleId)]) =
           q.dequeue
-        if node._1 > maxDepth then
+
+        if depth >= maxDepth then
           WikiResult.successful(None)
-        else if visited.contains(node._2) then
-          iter(visited, queue)
+
         else
-          val neighbors: WikiResult[Set[ArticleId]] = client.linksFrom(node._2)
-          val found: WikiResult[Boolean] =
-            neighbors.map(set => set.exists(id => id == target))
-          val nodes: WikiResult[Set[(Int, ArticleId)]] =
-            neighbors.map(set => set.map(aId => (node._1 + 1, aId)))
-          WikiResult.successful(None)
+          val neighbors: WikiResult[Set[ArticleId]] = client.linksFrom(aId)
+
+          val fun: Set[ArticleId] => WikiResult[Option[Int]] = nodes =>
+            if nodes.contains(target) then
+              WikiResult.successful(Some(depth))
+            else
+              val newVisited: Set[ArticleId] = visited + aId
+              val updatedNodes: Set[(Int, ArticleId)] =
+                nodes.map(aid => (depth + 1, aid))
+              iter(newVisited, queue.enqueueAll(updatedNodes))
+
+          neighbors
+            .flatMap(fun)
+            .fallbackTo(iter(visited + aId, queue))
+
     if start == target then WikiResult.successful(Some(0))
     else iter(Set(start), Queue(1->start))
 
@@ -93,18 +125,48 @@ final class Wikigraph(client: Wikipedia):
     * Hint: You should use the methods that you implemented on WikiResult as well as
     *       breadthFirstSearch
     */
-  def distanceMatrix(titles: List[String], maxDepth: Int = 50): WikiResult[Seq[(String, String, Option[Int])]] =
-  // traverse:
-  // as: Seq[A] = titles: List[String]
-  // so: A = String
-  // WikiResult[Seq[B]] = WikiResult[Seq[(String, String, Option[Int])]
-  // so: B = (String, String, Option[Int])
-  // we need: f: A => WikiResult[B]
-  // so: f: String => WikiResult[(String, String, Option[Int])]
-  // this can be done by zipping
-  //    WikiResult[String],
-  //    WikiResult[String],
-  //    WikiResult[Option[Int]] <- this is returned by breadthFirstSearch
-  //
-    ???
+  def distanceMatrix(titles: List[String], maxDepth: Int = 50)
+  : WikiResult[Seq[(String, String, Option[Int])]] = // TODO
+    // Let's play a game of Type Inference Puzzle!
+    // traverse:
+    // as: Seq[A] = titles: List[String]
+    // so: A = String
+    // WikiResult[Seq[B]] = WikiResult[Seq[(String, String, Option[Int])]
+    // so: B = (String, String, Option[Int])
+    // we need: f: A => WikiResult[B]
+    // so: f: String => WikiResult[(String, String, Option[Int])]
+    // this can be done by zipping
+    //    WikiResult[String],
+    //    WikiResult[String],
+    //    WikiResult[Option[Int]] <- this is returned by breadthFirstSearch
+    // We can also zip to get Seq[(String, String)] and use breadthFirstSearch.
+    // We look at pairs of titles from the given list. So we would need:
+    // f: (String, String) => WikiResult[(String, String, Option[Int])]
+    // First we can convert (String, String) to (ArticleId, ArticleId)
+    val titlePairs: Seq[(String, String)] =
+      for
+        source <- titles
+        dest <- titles
+        if source != dest
+      yield
+        (source, dest)
+
+    def fun(pair: (String, String)): WikiResult[(String, String, Option[Int])] =
+      val (source, dest): (String, String) = pair
+
+      val aIdPair: WikiResult[(ArticleId, ArticleId)] =
+        client.searchId(source).zip(client.searchId(dest))
+
+      def fun2(aidPair: (ArticleId, ArticleId)):
+      WikiResult[(String, String, Option[Int])] =
+        val (sourceId, destId) = aidPair
+        val res: WikiResult[Option[Int]] =
+          breadthFirstSearch(sourceId, destId, maxDepth)
+        res.flatMap(distance => WikiResult.successful(source, dest, distance))
+
+      aIdPair.flatMap(fun2)
+
+    WikiResult.traverse(titlePairs)(fun)
+
+
 end Wikigraph
