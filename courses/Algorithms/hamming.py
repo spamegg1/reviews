9f93ba71c7fba51c003b2b872b85b7cdb73d9c20
spamegg <spamegg@pm.me>
2022-01-01 22:19:37 +0300

    fix Hamming distance problem

--- courses/Algorithms/hamming.py
+++ courses/Algorithms/hamming.py
@@ -8,84 +8,41 @@ from collections import deque
 import unionfind as uf
 
 
-def hamming(codes, spacing):
+def hamming(codes, spacing, clusters):
     """Take a deque of 24-bit binary codes.
 
     Return largest k such that there is a k-clustering of the codes with
     at least given spacing.
     """
-    clusters = 0
-    firstdeque = codes
-    seconddeque = deque()
-
-    while firstdeque:
-        first = firstdeque.popleft()
-        cluster = deque([first])
-        while firstdeque:
-            second = firstdeque.popleft()
-            incluster = False
-            clusterlist = list(cluster)
-            for thing in clusterlist:
-                if hammingdistance(thing, second) < spacing:
-                    cluster.append(second)
-                    incluster = True
-                    break
-            if not incluster:
-                seconddeque.append(second)
-        clusters += 1
-        print(clusters)
-        firstdeque = seconddeque
-        seconddeque = deque()
-
-    return clusters
-
-
-def hamming2(codes, spacing):
-    """Take a deque of 24-bit binary codes.
-
-    Return largest k such that there is a k-clustering of the codes with
-    at least given spacing.
-    """
-    clusters = len(codes)
-    leftover = codes
-    while leftover:
-        newdeque = deque()
-        first = leftover.popleft()
-        cluster = deque([first])
-        cluster_changed = True
-        while cluster_changed:
-            cluster_changed = False
-            clusterlist = list(cluster)
-            while leftover:
-                second = leftover.popleft()
-                for node in clusterlist:
-                    if hammingdistance(second, node) < spacing:
-                        cluster.append(second)
-                        clusters -= 1
-                        print(clusters)
-                        cluster_changed = True
-                    else:
-                        newdeque.append(second)
-            leftover = newdeque
-    return clusters
-
-
-def hamming3(codes, spacing):
-    """Take a deque of 24-bit binary codes.
-
-    Return largest k such that there is a k-clustering of the codes with
-    at least given spacing.
-    """
-    clusters = len(codes)
+    result = clusters
     ufcodes = uf.UnionFind(codes)
     for code1 in codes:
-        for code2 in codes:
-            if ufcodes.find(code1) != ufcodes.find(code2):
-                if hammingdistance(code1, code2) < spacing:
-                    ufcodes.union_by_rank(code1, code2)
-                    clusters -= 1
-                    print(clusters)
-    return clusters
+        dist1 = dist1_codes({code1}, 24, {code1}) # 24
+        dist2 = dist1_codes(dist1, 24, {code1})   # 276
+        dist12 = dist1.union(dist2)               # 300
+        for code2 in dist12:
+            if code2 in codes:
+                if ufcodes.find(code1) != ufcodes.find(code2):
+                    if hammingdistance(code1, code2) < spacing:
+                        ufcodes.union_by_rank(code1, code2)
+                        result -= 1
+                        print(result)
+    return result
+
+
+def dist1_codes(codes, length, excluded):
+    """Take a set of Hamming codes of given length as input.
+
+    Return set of all possible Hamming codes that are
+    1 distance away from the given codes.
+    Codes in the given excluded variable are excluded.
+    """
+    result = deque()
+    for code in codes:
+        for i in range(length):
+            newcode = code[:i] + str((int(code[i]) + 1) % 2) + code[i+1:]
+            result.append(newcode)
+    return set(result) - excluded   
 
 
 def hammingdistance(code1, code2):
@@ -93,29 +50,28 @@ def hammingdistance(code1, code2):
 
     Return the Hamming distance between the codes.
     """
-    return bin(int(code1, 2) ^ int(code2, 2)).count('1')
+    return bin(int(code1, 2) ^ int(code2, 2)).count('1')    
 
 
-def codes_to_deque_list(filename, islist=False):
+def codes_to_deque_list(filename):
     """Take name of text file as input.
 
     Text file has, on each line, a Hamming code consisting of 24 bits.
     Each code is a string; the bits are separated by spaces.
-    Return a deque or a list of these codes (deque by default).
-    To return a list, set optional parameter islist to True.
+    Return a set of these codes.
     """
-    result = deque()
-    if islist:
-        result = []
+    result = set()    
     with open(filename, 'r') as file:
         for line in file:
             code = ''.join(line.split())
-            result.append(code)
+            result.add(code)
     return result
-
 
 # TESTING
 if __name__ == "__main__":
+    CLUSTERS = 200000
+    SPACING = 3
+    # removed the first line from the txt file (which said 200000)
     CODES = codes_to_deque_list('clustering_big.txt')
-    print(hamming(CODES, 3))  # 81120, 54179
+    print(hamming(CODES, SPACING, CLUSTERS))  # 6118
     print("Tests pass.")
--- courses/Algorithms/unionfind.py
+++ courses/Algorithms/unionfind.py
@@ -8,55 +8,92 @@ Created on Thu Oct 17 22:30:12 2019.
 
 class UnionFind:
     """Union-find (or disjoint-set) data structure."""
+    LEADER = 0
+    RANK = 1
 
-    def __init__(self, nodes):
-        """Take list of nodes as input, create union-find structure from it."""
-        self.nodes = nodes
-        self.parents = [i for i in range(len(nodes))]
-        self.sizes = [1 for _ in range(len(nodes))]
-        self.ranks = [0 for _ in range(len(nodes))]
+    def __init__(self, iterable):
+        # key = object,
+        # value = [leader of the set in which in the object, rank]
+        self.core = {i: [i, 0] for i in iterable}
 
-    def union_by_size(self, node1, node2):
-        """Merge the two sets containing given two nodes, by size."""
-        index1, index2 = self.find(node1), self.find(node2)
-        size1, size2 = self.sizes[index1], self.sizes[index2]
-        if index1 == index2:
-            return       # node1 and node2 belong to same set, nothing to merge
-        if size1 >= size2:   # add set containing node2 to set containing node1
-            self.parents[index2] = index1
-            self.sizes[index1] = size1 + size2
-        if size1 < size2:    # add set containing node1 to set containing node2
-            self.parents[index1] = index2
-            self.sizes[index2] = size1 + size2
+    def find(self, code):
+        code_copy = code
+        codes_for_path_compression = []
 
-    def union_by_rank(self, node1, node2):
-        """Merge the two sets containing given two nodes, by rank."""
-        index1, index2 = self.find(node1), self.find(node2)
-        rank1, rank2 = self.ranks[index1], self.ranks[index2]
-        if index1 == index2:
-            return       # node1 and node2 belong to same set, nothing to merge
-        if rank1 > rank2:    # add set containing node2 to set containing node1
-            self.parents[index2] = index1
-        if rank1 < rank2:    # add set containing node1 to set containing node2
-            self.parents[index1] = index2
-        if rank1 == rank2:   # add set containing node1 to set containing node2
-            self.parents[index1] = index2
-            self.ranks[index2] += 1  # this is the only case with a rank change
+        leader = self.core[code_copy][self.LEADER]
+        while code_copy != leader:
+            codes_for_path_compression.append(code_copy)
+            code_copy = leader
+            leader = self.core[code_copy][self.LEADER]
 
-    def find(self, node):
-        """Return index of set that contains given node."""
-        index = self.nodes.index(node)
-        path = []
-        while True:
-            if self.parents[index] == index:
-                for child in path:
-                    self.parents[child] = index              # path compression
-                return index
-            path.append(index)
-            index = self.parents[index]                               # move up
+        for i in codes_for_path_compression:
+            self.core[i][self.LEADER] = leader
+
+        return leader
+
+    def union_by_rank(self, code1, code2):
+
+        leader1 = self.find(code1)
+        leader2 = self.find(code2)
+        rank1 = self.core[leader1][self.RANK]
+        rank2 = self.core[leader2][self.RANK]
+
+        if rank1 < rank2:
+            self.core[leader1][self.LEADER] = leader2
+        elif rank1 > rank2:
+            self.core[leader2][self.LEADER] = leader1
+        elif rank1 == rank2:
+            self.core[leader1][self.LEADER] = leader2
+            self.core[leader2][self.RANK] += 1
+
+##    def __init__(self, nodes):
+##        """Take list of nodes as input, create union-find structure from it."""
+##        self.nodes = nodes
+##        self.parents = [i for i in range(len(nodes))]
+##        self.sizes = [1 for _ in range(len(nodes))]
+##        self.ranks = [0 for _ in range(len(nodes))]
+##
+##    def union_by_size(self, node1, node2):
+##        """Merge the two sets containing given two nodes, by size."""
+##        index1, index2 = self.find(node1), self.find(node2)
+##        size1, size2 = self.sizes[index1], self.sizes[index2]
+##        if index1 == index2:
+##            return       # node1 and node2 belong to same set, nothing to merge
+##        if size1 >= size2:   # add set containing node2 to set containing node1
+##            self.parents[index2] = index1
+##            self.sizes[index1] = size1 + size2
+##        if size1 < size2:    # add set containing node1 to set containing node2
+##            self.parents[index1] = index2
+##            self.sizes[index2] = size1 + size2
+##
+##    def union_by_rank(self, node1, node2):
+##        """Merge the two sets containing given two nodes, by rank."""
+##        index1, index2 = self.find(node1), self.find(node2)
+##        rank1, rank2 = self.ranks[index1], self.ranks[index2]
+##        if index1 == index2:
+##            return       # node1 and node2 belong to same set, nothing to merge
+##        if rank1 > rank2:    # add set containing node2 to set containing node1
+##            self.parents[index2] = index1
+##        if rank1 < rank2:    # add set containing node1 to set containing node2
+##            self.parents[index1] = index2
+##        if rank1 == rank2:   # add set containing node1 to set containing node2
+##            self.parents[index1] = index2
+##            self.ranks[index2] += 1  # this is the only case with a rank change
+##
+##    def find(self, node):
+##        """Return index of set that contains given node."""
+##        index = self.nodes.index(node)
+##        path = []
+##        while True:
+##            if self.parents[index] == index:
+##                for child in path:
+##                    self.parents[child] = index              # path compression
+##                return index
+##            path.append(index)
+##            index = self.parents[index]                               # move up
 
 
 # TESTING
 if __name__ == "__main__":
     assert True
-    print("tests pass")
+    print("Tests pass.")
