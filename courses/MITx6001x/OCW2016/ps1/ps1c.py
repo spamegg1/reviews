0cfb83e48acb2e80ae8d0b2196deaa9f57f7e5c2
spamegg <spamegg@pm.me>
2022-03-28 11:32:17 +0300

    refactor `ps1c.py` in OCW2016

--- courses/MITx6001x/OCW2016/ps1/ps1c.py
+++ courses/MITx6001x/OCW2016/ps1/ps1c.py
@@ -1,57 +1,58 @@
 # Constants given to us by the problem
-semi_annual_raise = 0.07
-investment_rate = 0.04
-total_cost = 1000000
-down_payment = total_cost * 0.25
+SEMI_ANNUAL_RAISE = 0.07
+INV_RATE = 0.04
+TOTAL_COST = 1000000
+DOWN_PAYMENT = TOTAL_COST * 0.25
 
 
-def savings(starting_salary, portion_saved):
-    months = 0
-    current_savings = 0
-    salary = starting_salary
+def savings(start, portion, inv_rate, semi_raise):
+    saving = 0
+    salary = start
 
-    while months < 36:
+    for months in range(36):
         if months > 0 and months % 6 == 0:
-            annual_raise = salary * semi_annual_raise
-            salary = salary + annual_raise
+            salary += salary * semi_raise
 
-        investment_return = current_savings * investment_rate / 12
-        current_savings += salary * portion_saved / 120000
-        current_savings += investment_return
+        inv_return = saving * inv_rate / 12
+        saving += salary * portion / 120000
+        saving += inv_return
 
-        months += 1
-
-    return current_savings
+    return saving
 
 
-def optimal_saving_rate():
+def bisection_search(salary, inv_rate, semi_raise, down_payment):
     # Variables for bisection search
-    left = 0
-    right = 10000
+    left, right = 0, 10000
     portion_saved = 5000
     steps_in_search = 0
 
-    starting_salary = float(input("Enter the starting salary: "))
+    # do-while
+    saving = savings(salary, portion_saved, inv_rate, semi_raise)
+    steps_in_search += 1
 
-    if savings(starting_salary, 10000) < down_payment:
+    while abs(saving - down_payment) >= 100.0:
+        if saving < down_payment:
+            left = portion_saved
+        else:
+            right = portion_saved
+
+        portion_saved = (left + right) // 2
+        steps_in_search += 1
+        saving = savings(salary, portion_saved, inv_rate, semi_raise)
+
+    print("Best savings rate:", portion_saved / 10000)
+    print("Steps in bisection search:", steps_in_search)
+
+
+def optimal_saving_rate(salary, inv_rate, semi_raise, down_payment):
+    max_saving = savings(salary, 10000, inv_rate, semi_raise)
+
+    if max_saving < down_payment:
         print("It is not possible to pay the down payment in three years.")
     else:
-        current_savings = savings(starting_salary, portion_saved)
-        steps_in_search += 1
-
-        while abs(current_savings - down_payment) >= 100.0:
-            if current_savings < down_payment:
-                left = portion_saved
-            else:
-                right = portion_saved
-
-            portion_saved = (left + right) // 2
-            steps_in_search += 1
-            current_savings = savings(starting_salary, portion_saved)
-
-        print("Best savings rate:", portion_saved / 10000)
-        print("Steps in bisection search:", steps_in_search)
+        bisection_search(salary, inv_rate, semi_raise, down_payment)
 
 
 if __name__ == "__main__":
-    optimal_saving_rate()
+    SALARY = float(input("Enter the starting salary: "))
+    optimal_saving_rate(SALARY, INV_RATE, SEMI_ANNUAL_RAISE, DOWN_PAYMENT)
