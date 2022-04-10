debc7f987e6345baaf5780a6cb60202734532de7
spamegg <spamegg@pm.me>
2022-04-10 21:05:07 +0300

    cs50/2022-spring/w6/lab6

--- courses/CS50/2022-spring/w6/lab6/tournament.py
+++ courses/CS50/2022-spring/w6/lab6/tournament.py
@@ -16,13 +16,20 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
+    with open(sys.argv[1]) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            teams.append({'team': row['team'], 'rating': int(row['rating'])})
 
-    counts = {}
-    # TODO: Simulate N tournaments and keep track of win counts
+        counts = {}
+        # TODO: Simulate N tournaments and keep track of win counts
+        for _ in range(N):
+            winner = simulate_tournament(teams)
+            counts[winner] = counts.get(winner, 0) + 1
 
-    # Print each team's chances of winning, according to simulation
-    for team in sorted(counts, key=lambda team: counts[team], reverse=True):
-        print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
+        # Print each team's chances of winning, according to simulation
+        for team in sorted(counts, key=lambda team: counts[team], reverse=True):
+            print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
 
 
 def simulate_game(team1, team2):
@@ -50,6 +57,11 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
+    winners = simulate_round(teams)
+    while len(winners) > 1:
+        winners = simulate_round(winners)
+
+    return winners[0]['team']
 
 
 if __name__ == "__main__":
