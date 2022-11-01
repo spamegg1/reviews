810831ed9d3bf81fb2af40a7acee98d5809d0a0b
spamegg <spamegg@pm.me>
2022-11-02 01:40:54 +0300

    MfCS in-class 19

--- courses/ProgLangB/week1/100_memoization.rkt
+++ courses/ProgLangB/week1/100_memoization.rkt
@@ -21,16 +21,26 @@
         (f 1 1 3))))
 
 (define fibonacci3
-  (letrec([memo null] ; list of pairs (arg . result) 
+  (letrec([memo null] ; list of pairs (arg . result)
           [f (lambda (x)
                (let ([ans (assoc x memo)])
-                 (if ans 
+                 (if ans
                      (cdr ans)
                      (let ([new-ans (if (or (= x 1) (= x 2))
                                         1
                                         (+ (f (- x 1))
                                            (f (- x 2))))])
-                       (begin 
+                       (begin
                          (set! memo (cons (cons x new-ans) memo))
                          new-ans)))))])
     f))
+
+;real    0m0,584s
+;user    0m0,519s
+;sys     0m0,064s
+(fibonacci2 100000)
+
+;real    0m1,019s
+;user    0m0,677s
+;sys     0m0,340s
+(fibonacci3 100000)
