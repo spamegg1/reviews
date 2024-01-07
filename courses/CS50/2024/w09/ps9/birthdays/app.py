1f2a289e2a7b78a6fb968643ca87bd2abfa85580
spamegg1 <spamegg@pm.me>
2024-01-07 20:06:50 +0300

    CS50 January 2024
    - weeks 09-10

---
+++ courses/CS50/2024/w09/ps9/birthdays/app.py
@@ -0,0 +1,41 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///birthdays.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        # TODO: Add the user's entry into the database
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES (:name, :month, :day)",
+                   name=name, month=month, day=day)
+        return redirect("/")
+
+    else:
+        # TODO: Display the entries in the database on index.html
+        birthdays = db.execute("SELECT * FROM birthdays;")  # list of dicts
+        return render_template("index.html", birthdays=birthdays)
+
+
